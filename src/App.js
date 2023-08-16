0046e07bea22c8881708c49aba3c7578c60fd27c
InshaY <insha@test.com>
2023-08-17 01:37:23 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
 
-function App() {
+
+const App = () => {
+// In useState first value is current value and we store it into variable name myNAME and then other value it take is function and then we define it setMyname when this one function and here we give new value because we define this one function on inside the fucntion that call when we clicked on the button 
+ const[myNAME, setMyname ] = useState("INSHA YAQOOB")
+  const changestatement = () =>{
+    setMyname("BY click on it changed to INSHA")
+
+  }
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "gray"
+  };
+
+  const headingStyle = {
+    margin: 0,
+    textAlign: "center"
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#007BFF",
+    color: "white",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "5px",
+    marginTop: "10px",
+    cursor: "pointer"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>{myNAME}</h1>
+      <button style={buttonStyle} onClick={changestatement}>Click me plz</button>
     </div>
   );
 }
-
 export default App;
